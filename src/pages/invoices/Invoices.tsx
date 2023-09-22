5632ecb6f91ccea5cf9096f30d43f34320dd2e52
Mario <mariovidovic97@gmail.com>
2023-09-22 11:27:51 +0200

    Bug fixes

--- src/pages/invoices/Invoices.tsx
+++ src/pages/invoices/Invoices.tsx
@@ -3,7 +3,6 @@ import { useCallback, useMemo, useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { AppDispatch, RootState } from "@src/store/store";
-import { subDays, subHours } from "date-fns";
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
 import Container from "@mui/material/Container";
